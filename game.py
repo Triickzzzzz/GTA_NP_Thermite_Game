2481112ee0acdc6ff998e074decfa44d05a2d298
Triickzzzzz <Kevin924mx.de>
2020-03-17 23:43:45 +0100

    init board labels

--- game.py
+++ game.py
@@ -12,12 +12,23 @@ class Game(tk.Frame):
         helv12 = tkFont.Font(family="Helvetica",size=12,weight="bold")
         helv20 = tkFont.Font(family="Helvetica",size=20,weight="bold")
 
+        #=====Main objects=====#
         lbl_head = tk.Label(self, bg="white", text="Proof Your Skill", font=helv12)
-        frame_board = tk.Frame(self, bg="black")
+        frame_board = tk.Frame(self, bg="white")
         lbl_fails = tk.Label(self, bg="white", text="[ - ]  [ - ]  [ - ]", font=helv20)
 
         lbl_head.pack(fill=tk.X)
         frame_board.pack(fill=tk.BOTH, expand=True)
         lbl_fails.pack(fill=tk.X)
-        
+
+        #=====Board objects=====#        
+        lbl_blue = tk.Label(frame_board, bg="RoyalBlue1")
+        lbl_red = tk.Label(frame_board, bg="tomato")
+        lbl_green = tk.Label(frame_board, bg="medium sea green")
+
+        lbl_blue.pack(fill=tk.BOTH, expand=True, side=tk.LEFT, padx=(10,5), pady=10)
+        lbl_red.pack(fill=tk.BOTH, expand=True, side=tk.LEFT, padx=(5,5), pady=10)
+        lbl_green.pack(fill=tk.BOTH, expand=True, side=tk.LEFT, padx=(5,10), pady=10)
+
+
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from game import Game
 app = tk.Tk()
 app.title("Thermite Game")
 app.geometry("400x600")
-app.resizable(True, True)
+app.resizable(False, False)
 
 g_frame = Game(app)
 app.mainloop()
